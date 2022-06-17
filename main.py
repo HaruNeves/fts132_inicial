898c77e5c5eda338a263674a7ca2518b9eda378b
Haru <haru.neves@gmail.com>
2022-06-17 15:24:19 -0300

    criação da função @calcular_volume_do_paralelograma e seu respectivo teste unitário

--- main.py
+++ main.py
@@ -1,33 +1,69 @@
 import pytest
 
-#Definições
-def somar(n1, n2):
-    return n1 + n2
+    # Cozinheiro - Definições
+def somar(num1, num2):
+    return num1 + num2
 
-def subtrair(n1, n2):
-    return n1 - n2
+def subtrair(num1, num2):
+    return num1 - num2
 
-def multiplicar(n1, n2):
-    return n1 * n2
+def multiplicar(num1, num2):
+    return num1 * num2
 
-def dividir(n1, n2):
+def dividir(num1, num2):
     try:
-        return n1 / n2
+        return num1 / num2
     except ZeroDivisionError:
         return 'Não é possível dividir por zero'
 
+def elevar(num1, num2):
+    return num1 ** num2
 
-#Requisições
+
+def calcular_area_do_circulo(raio):
+    try:
+        return 3.14 * raio ** 2
+    except TypeError:
+        return 'Falha no calculo - Raio não é um número'
+
+def calcular_volume_do_paralelograma(largura, comprimento, altura):
+    return largura * comprimento * altura
+
+
 if __name__ == '__main__':
-    resultado = somar(5, 7)
-    print(f'A soma é {resultado}')
 
-    resultado = subtrair(7, 5)
+    # Garçon - Requisições / Chamadas
+    resultado = somar_dois_numeros(5,7)
+    print(f'A soma é {resultado}') # <-- Prato
+
+    resultado = subtrair_dois_numeros(7,5)
     print(f'A subtração é {resultado}')
 
-    resultado = multiplicar(5, 7)
+    resultado = multiplicar_dois_numeros(3,5)
     print(f'A multiplicação é {resultado}')
 
-    resultado = dividir(7, 2)
+    resultado = dividir_dois_numeros(8,0)
     print(f'A divisão é {resultado}')
 
+    resultado = elevar_um_numero_pelo_outro(2,3)
+    print(f'A exponenciação é {resultado}')
+
+
+
+
+    # Degustador / Teste
+
+def testar_somar_dois_numeros():
+    # - 1ª Etapa: Configura / Prepara
+    # Dados / Valores
+    # Entrada / Input
+    num1 = 8
+    num2 = 9
+    # Saída / Output
+    resultado_esperado = 17
+
+    # - 2ª Etapa: Executa
+    resultado_atual = somar_dois_numeros(num1, num2)
+
+    # - 3ª Etapa: Confirma / Check / Valida
+    assert resultado_atual == resultado_esperado
--- test/unit/test_main.py
+++ test/unit/test_main.py
@@ -1,6 +1,6 @@
 import pytest
 
-from main import somar, subtrair, multiplicar, dividir
+from main import somar, subtrair, multiplicar, dividir, calcular_area_do_circulo, calcular_volume_do_paralelograma
 
 
 def testar_soma():
@@ -19,6 +19,7 @@ def testar_soma():
     # 3 Etapa - Confirma / Check / Valida
     assert resultado_atual == resultado_esperado
 
+
 def testar_subtracao():
     n1 = 10
     n2 = 5
@@ -26,12 +27,14 @@ def testar_subtracao():
     resultado_atual = subtrair(n1, n2)
     assert resultado_atual == resultado_esperado
 
+
 def testar_multiplicacao():
     n1 = 2
     n2 = 2
     resultado_esperado = 4
     resultado_atual = multiplicar(n1, n2)
     assert resultado_atual == resultado_esperado
+
 
 def testar_divisao():
     n1 = 10
@@ -41,3 +44,31 @@ def testar_divisao():
     assert resultado_atual == resultado_esperado
 
 
+# anotação par utilizar como massa de teste
+@pytest.mark.parametrize('raio, resultado_esperado', [
+    # valores
+    (1, 3.14),  # teste número 1
+    (2, 12.56),  # teste número 2
+    (3, 28.26), # teste número 3
+    (4, 50.24),  #teste número 4
+    ('a', 'Falha no calculo - Raio não é um número'), #teste número 5, teste negativo
+    (' ', 'Falha no calculo - Raio não é um número'),  # teste número 6, teste negativo
+])
+def testar_calcular_area_do_circulo(raio, resultado_esperado):
+    # 1 - Configura / Comentamos para que os parametros sejam lidos
+    # raio = 2
+    # resultado_esperado = 12.56
+
+    # 2 - Executa
+    resultado_atual = calcular_area_do_circulo(raio)
+
+    # 3 - Valida
+    assert resultado_atual == resultado_esperado
+
+def testar_calcular_volume_do_paralelograma():
+    largura = 5
+    comprimento = 10
+    altura = 2
+    resultado_esperado = 100
+    resultado_atual = calcular_volume_do_paralelograma(largura, comprimento, altura)
+    assert resultado_atual == resultado_esperado
